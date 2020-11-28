f7f1700fb055a34c787dca859b2fcba1496b5f2c
Lida-Ghaemi <74499752+Lida-Ghaemi@users.noreply.github.com>
2020-11-28 12:45:34 -0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -5,9 +5,9 @@
 # test_data: by default, points to the validation set, since this is the set that
 #   will be evaluated after each training iteration. If you wish to test
 #   on the final (held-out) test set, change 'val' to 'test'.
-type=java-large-model
-dataset_name=java-large
-data_dir=data/java-large
+type=java-small-model
+dataset_name=java-small
+data_dir=data/java-small
 data=${data_dir}/${dataset_name}
 test_data=${data_dir}/${dataset_name}.val.c2s
 model_dir=models/${type}
@@ -15,3 +15,15 @@ model_dir=models/${type}
 mkdir -p ${model_dir}
 set -e
 python3 -u code2seq.py --data ${data} --test ${test_data} --save_prefix ${model_dir}/model
+
+#Before 
+#type=java-large-model
+#dataset_name=java-large
+#data_dir=data/java-large
+#data=${data_dir}/${dataset_name}
+#test_data=${data_dir}/${dataset_name}.val.c2s
+#model_dir=models/${type}
+
+#mkdir -p ${model_dir}
+#set -e
+#python3 -u code2seq.py --data ${data} --test ${test_data} --save_prefix ${model_dir}/model
