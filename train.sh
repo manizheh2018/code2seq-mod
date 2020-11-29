43e2f8825f3884afa6b9aaae493a70568266c852
Lida-Ghaemi <74499752+Lida-Ghaemi@users.noreply.github.com>
2020-11-29 05:11:22 -0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -5,9 +5,9 @@
 # test_data: by default, points to the validation set, since this is the set that
 #   will be evaluated after each training iteration. If you wish to test
 #   on the final (held-out) test set, change 'val' to 'test'.
-type=java-small-model
-dataset_name=java-small
-data_dir=data/java-small
+type=java-large-model
+dataset_name=java-large
+data_dir=data/java-large
 data=${data_dir}/${dataset_name}
 test_data=${data_dir}/${dataset_name}.val.c2s
 model_dir=models/${type}
