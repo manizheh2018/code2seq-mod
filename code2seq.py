db2e2004322e9011eb857d4f19058733826cd177
Lida-Ghaemi <74499752+Lida-Ghaemi@users.noreply.github.com>
2020-12-07 08:56:08 +0330

    Update code2seq.py

--- code2seq.py
+++ code2seq.py
@@ -3,7 +3,7 @@ import numpy as np
 #%tensorflow_version 1.x
 import tensorflow as tf
 #import geneticalgorithm as ga
-#from geneticalgorithm import geneticalgorithm as ga
+from geneticalgorithm import geneticalgorithm as ga
 from config import Config
 from interactive_predict import InteractivePredictor
 from model import Model
@@ -18,7 +18,7 @@ def evaluate_each_indiv(config,i):
         model.train2()
         #if args.data_path:
         results, precision, recall, f1, rouge = model.evaluate()
-        #print("i am out of evaluate$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+       # print("i am out of evaluate$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
         print('Accuracy: ' + str(results))
         print('Precision: ' + str(precision) + ', recall: ' + str(recall) + ', F1: ' + str(f1))
         print('Rouge: ', rouge)
@@ -26,7 +26,7 @@ def evaluate_each_indiv(config,i):
         model.train1()
         #if args.data_path:
         results, precision, recall, f1, rouge = model.evaluate()
-        #print("i am out of evaluate$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+       # print("i am out of evaluate$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
         print('Accuracy: ' + str(results))
         print('Precision: ' + str(precision) + ', recall: ' + str(recall) + ', F1: ' + str(f1))
         print('Rouge: ', rouge)
@@ -36,7 +36,7 @@ def evaluate_each_indiv(config,i):
       #  predictor.predict()
    # if args.release and args.load_path:
      #   model.evaluate(release=True)
-   # print("i have finished f111111111yyyyyyyyy")
+    print("i have finished f111111111yyyyyyyyy")
     model.close_session()
     return f1
 ####################################################
@@ -60,13 +60,6 @@ def mymutate(indiv,ind):
      # indiv[2]=np.random.randint(1,high=4,dtype=int)
     else: 
       indiv[2]=np.random.randint(1,high=11,dtype=int)
-      config.BATCH_SIZE=indiv[0]
-      #config.RNN_SIZE =indiv[1]*2
-      config.NUM_EPOCHS =indiv[1]
-      #config.NUM_DECODER_LAYERS=indiv[2]
-      config.MAX_TARGET_PARTS=indiv[2]
-      #model = Model(config)
-      indiv[3]=evaluate_each_indiv(config,1)
     #print(indiv)
     return indiv
 ###############################################################
@@ -101,7 +94,7 @@ def initialize_pop(popsize,n_var,config):
     print("initialization finished")
     return pop
 ##################################################
-def mycross(pop,cross_p,popsize,n_var):
+def mycross(pop,cross_p,popsize):
   for i in range(np.math.ceil(popsize/2)):
     r=np.random.randint(low=0,high=popsize, size=2,dtype=int)#crossover selection for two indivi
     cpoint=np.random.randint(low=1,high=n_var, size=1,dtype=int)# croxover point
@@ -109,21 +102,8 @@ def mycross(pop,cross_p,popsize,n_var):
     temp2=pop[r[1]]
     temp1[cpoint[0]:]=pop[r[1]][cpoint[0]:]
     temp2[cpoint[0]:]=pop[r[0]][cpoint[0]:]
-    config.BATCH_SIZE=temp1[0]
-      #config.RNN_SIZE =indiv[1]*2
-    config.NUM_EPOCHS =temp1[1]
-      #config.NUM_DECODER_LAYERS=indiv[2]
-    config.MAX_TARGET_PARTS=temp1[2]
-      #model = Model(config)
-   
-    temp1[n_var]=evaluate_each_indiv(config,1)
-    config.BATCH_SIZE=temp2[0]
-      #config.RNN_SIZE =indiv[1]*2
-    config.NUM_EPOCHS =temp2[1]
-      #config.NUM_DECODER_LAYERS=indiv[2]
-    config.MAX_TARGET_PARTS=temp2[2]
-      #model = Model(config)
-    temp2[n_var]=evaluate_each_indiv(config,1)
+    temp1[n_var]=2
+    temp2[n_var]=4
     
     if temp1[n_var]>pop[r[0]][n_var]:
       pop[r[0]]=temp1
@@ -155,13 +135,13 @@ if __name__ == '__main__':
     tf.set_random_seed(args.seed)
     #tf.random.set_seed(args.seed)
     ############################################################
-    #print(args.debug)
+    print(args.debug)
 
     if args.debug:
         config = Config.get_debug_config(args)
     else:
         config = Config.get_default_config(args)
-    #print(config.BATCH_SIZE)
+    print(config.BATCH_SIZE)
     ###GA
     #varbound=np.array([[0,10]]*3)
     #modelga=ga(function=f,dimension=3,variable_type='real',variable_boundaries=varbound)
@@ -173,56 +153,15 @@ if __name__ == '__main__':
     popsize=5
     pop=initialize_pop(popsize,n_var+1,config)
     print(pop)
-    n_iters=7
+    n_iters=10
     p_mutate=0.3
-    cross_p=0.6
-    for i in range(n_iters):
-        pop=mycross(pop,cross_p,popsize,n_var)# crossover
-        for j in range(popsize):#loop for muration
-            r=np.random.random(1)
-            if r<p_mutate:
-                temp=mymutate(pop[j], np.random.randint(4))
-                if temp[n_var]>pop[j][n_var]:
-                    pop[j]=temp
-    pop = sorted(pop, key = lambda x:x[n_var-1]) 
-    print(pop)
-    best=pop[-1]
+    cross_p=0.7
 
-    config.BATCH_SIZE=best[0]
-      #config.RNN_SIZE =indiv[1]*2
-    config.NUM_EPOCHS =best[1]
-      #config.NUM_DECODER_LAYERS=indiv[2]
-    config.MAX_TARGET_PARTS=best[2]
-      #model = Model(config)
-
-     #def print_hyperparams(self):
-    print('Training batch size:\t\t\t', config.BATCH_SIZE)
-    print('Epochs:\t\t', config.NUM_EPOCHS)
-    print('Max target length:\t\t\t', config.MAX_TARGET_PARTS)
-    print('Dataset path:\t\t\t\t', config.TRAIN_PATH)
-    print('Training file path:\t\t\t', config.TRAIN_PATH + '.train.c2s')
-    print('Validation path:\t\t\t', config.TEST_PATH)
-    print('Taking max contexts from each example:\t', config.MAX_CONTEXTS)
-    print('Random path sampling:\t\t\t', config.RANDOM_CONTEXTS)
-    print('Embedding size:\t\t\t\t', config.EMBEDDINGS_SIZE)
-    if config.BIRNN:
-        print('Using BiLSTMs, each of size:\t\t', config.RNN_SIZE // 2)
-    else:
-        print('Uni-directional LSTM of size:\t\t', config.RNN_SIZE)
-    print('Decoder size:\t\t\t\t', config.DECODER_SIZE)
-    print('Decoder layers:\t\t\t\t', config.NUM_DECODER_LAYERS)
-    print('Max path lengths:\t\t\t', config.MAX_PATH_LENGTH)
-    print('Max subtokens in a token:\t\t', config.MAX_NAME_PARTS)
-    print('Embeddings dropout keep_prob:\t\t', config.EMBEDDINGS_DROPOUT_KEEP_PROB)
-    print('LSTM dropout keep_prob:\t\t\t', config.RNN_DROPOUT_KEEP_PROB)
-    print('============================================') 
+      
     #aa=evaluate_each_indiv(model,config)
-    #print("heyyyyyyyyyyyyyyyyy I am starting main train\n")
+    print("heyyyyyyyyyyyyyyyyy I am starting main train\n")
     
     model = Model(config)
-    print("\n************************************* this is the config to train ************************************\n ")
-    print(config.BATCH_SIZE,config.NUM_EPOCHS ,config.MAX_TARGET_PARTS)
-      #model = Model(config)
     print('Created model')
     if config.TRAIN_PATH:
         model.train()
